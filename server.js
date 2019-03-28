f269e85be65a4db3a6125642ec35720783a9a5ad
Xiao Liu <liuxiaoabc@gmail.com>
2019-03-28 14:11:47 +0800

    add proxy

--- server.js
+++ server.js
@@ -5,12 +5,28 @@ const config = require('./src/config')
 const logger = require('./src/middleware/logger')
 const xmlParse = require('./src/middleware/xmlParse')
 const router = require('./src/router')
+const httpProxy = require('http-proxy-middleware');
+const k2c = require('koa2-connect');
 
 const app = new Koa()
 
 app
   .use(logger())
   .use(static(path.join( __dirname, './public')))
+  .use(async(ctx, next) => {    
+    if (ctx.url.startsWith('/grpc')) { //匹配有api字段的请求url       
+      ctx.respond = false // 绕过koa内置对象response ，写入原始res对象，而不是koa处理过的response        
+      await k2c(httpProxy({        
+        target: 'http://f9265be8.ngrok.io',         
+        changeOrigin: true,        
+        secure: false,        
+        pathRewrite: {        
+          '^/grpc': ''            
+        }        
+      }))(ctx,next);    
+    }    
+    await next();
+  })
   .use(xmlParse())
   .use(router.routes())
   .use(router.allowedMethods())
